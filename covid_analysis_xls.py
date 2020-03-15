7122bd257b50039d32857af37e141ec8476d5393
fpedaci <fpedaci@gmail.com>
2020-03-15 16:08:45 +0100

    added per day

--- covid_analysis_xls.py
+++ covid_analysis_xls.py
@@ -19,12 +19,18 @@ def covid_analysis_xls(countries=['Italy','France'], download=False, fitpts=10, 
             os.rename('data.xls', 'data_old.xls')
         wget.download('https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-2020-03-14_1.xls', './data.xls')
 
+    d_cases_day = {}
+    d_deaths_day = {}
     d_cases = {}
     d_deaths = {}
     
-    fig = plt.figure('covid_xls', clear=True)
-    ax1 = fig.add_subplot(211)
-    ax2 = fig.add_subplot(212, sharex=ax1)
+    fig = plt.figure('covid_xls cumulative', clear=True)
+    ax1 = fig.add_subplot(121)
+    ax2 = fig.add_subplot(122, sharex=ax1)
+
+    fig1 = plt.figure('covid_xls per day', clear=True)
+    ax11 = fig1.add_subplot(121)
+    ax12 = fig1.add_subplot(122, sharex=ax11)
     
     # get data from data.xls:
     book = xlrd.open_workbook('data.xls')
@@ -40,9 +46,14 @@ def covid_analysis_xls(countries=['Italy','France'], download=False, fitpts=10, 
         # populate with dates_float, data:
         c = np.array([[float(i[1]), float(i[2])] for i in cases_all if i[0]==country])
         d = np.array([[float(i[1]), float(i[2])] for i in deaths_all if i[0]==country])
-        d_cases[country] = c[::-1]
-        d_deaths[country] = d[::-1]
+        
+        # per day: 
+        d_cases_day[country] = np.copy(c[::-1])
+        d_deaths_day[country] = np.copy(d[::-1])
+
         # cumulative:
+        d_cases[country] = np.copy(c[::-1])
+        d_deaths[country] = np.copy(d[::-1])
         d_cases[country][:,1] = np.cumsum(d_cases[country][:,1])
         d_deaths[country][:,1] = np.cumsum(d_deaths[country][:,1])
 
@@ -57,24 +68,39 @@ def covid_analysis_xls(countries=['Italy','France'], download=False, fitpts=10, 
         dates_time_d = [str(datetime.datetime(*xlrd.xldate_as_tuple(a, book.datemode)).date()) for a in d_deaths[country][:,0]]
 
         # plots: 
-        xfit1c = np.arange(xfit0c[0], xfit0c[-1]+fitpts_ext)
-        xfit1d = np.arange(xfit0d[0], xfit0d[-1]+fitpts_ext)
-        p1, = ax1.semilogy(d_cases[country][:,0], d_cases[country][:,1], 'o', alpha=0.3, label=country)
-        p2, = ax2.semilogy(d_deaths[country][:,0], d_deaths[country][:,1], 'o', alpha=0.3, label=country)
-        ax1.plot(xfit1c, 10**np.poly1d(logfit_cases)(xfit1c), '--', color=p1.get_color())
-        ax2.plot(xfit1d, 10**np.poly1d(logfit_deaths)(xfit1d), '--', color=p2.get_color())
+        p1, = ax1.semilogy(d_cases[country][:,0], d_cases[country][:,1], '-o', alpha=0.4, label=country)
+        p2, = ax2.semilogy(d_deaths[country][:,0], d_deaths[country][:,1], '-o', alpha=0.4, label=country)
+        
+        xfit1c = np.arange(xfit0c[0], xfit0c[-1] + fitpts_ext)
+        xfit1d = np.arange(xfit0d[0], xfit0d[-1] + fitpts_ext)
+        ax1.plot(xfit1c, 10**np.poly1d(logfit_cases)(xfit1c), '--', alpha=0.6, color=p1.get_color())
+        ax2.plot(xfit1d, 10**np.poly1d(logfit_deaths)(xfit1d), '--', alpha=0.6, color=p2.get_color())
+        
+        p11, = ax11.semilogy(d_cases_day[country][:,0], d_cases_day[country][:,1], '-o', alpha=0.4, label=country)
+        p12, = ax12.semilogy(d_deaths_day[country][:,0], d_deaths_day[country][:,1], '-o', alpha=0.4, label=country)
+
     
     ax1.legend()
     ax2.legend()
+    ax11.legend()
+    ax12.legend()
     ax1.set_xlabel('Days')
     ax2.set_xlabel('Days')
     ax1.set_ylabel('Cumulative cases')
     ax2.set_ylabel('Cumulative deaths')
-    plt.tight_layout()
+    ax11.set_xlabel('Days')
+    ax12.set_xlabel('Days')
+    ax11.set_ylabel('Daily cases')
+    ax12.set_ylabel('Daily deaths')
+    fig.tight_layout()
+    fig1.tight_layout()
 
     from matplotlib.ticker import FuncFormatter
     formatter = FuncFormatter(lambda x_val, tick_pos: str(datetime.datetime(*xlrd.xldate_as_tuple(x_val, book.datemode)).date()).lstrip('2020'))
+    ax1.xaxis.set_major_formatter(formatter)
     ax2.xaxis.set_major_formatter(formatter)
+    ax11.xaxis.set_major_formatter(formatter)
+    ax12.xaxis.set_major_formatter(formatter)
 
 
     #return d_cases, d_deaths
